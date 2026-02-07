7549265c8d2b8ba6a40f8ae28049a8685d483b44
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2026-02-07 13:39:52 +0100

    comment: fix 'dev' command

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
   },
   define: {
     // Prevent Vite from injecting process.env like Webpack DefinePlugin
-    "process.env": "window.process.env",
+    "process.env": "window?.process?.env",
   },
   resolve: {
     alias: [
